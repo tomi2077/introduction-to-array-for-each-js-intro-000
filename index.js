cc92a7c8543e5f83d30d31148f172013937afcfa
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-25 01:52:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var doToElementsInArray = (array, callback) => {
   array = ["apple", "banana", "cherry"];
   callback = function(fruit) {
     return `Mmmm, ${fruit}!!!`;
-  array.forEach(callback);
   }
+  array.forEach(callback);
 }
 
 
