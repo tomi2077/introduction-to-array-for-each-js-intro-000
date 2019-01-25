a6e341ad736b3c00f48822d6ce29f09e3c846e50
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-25 01:48:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
-var doToElementsInArray = () => {
-  
+var doToElementsInArray = (array, callback) => {
+  array = ["apple", "banana", "cherry"];
+  callback = function(fruit) {
+    return `Mmmm, ${fruit}!!!`;
+  array.forEach(callback);
+  }
 }
-
-const array = ["apple", "banana", "cherry"];
-
-array.forEachconst callback =  functon()
-array.forEach(doToElementsInArray())
-  
-
- it('invokes the passed-in callback function on every element of the passed-in array using Array.prototype.forEach()', () => {
-      const callback = function(fruit) {
-        return `Mmmm, ${fruit}!!!`;
-      };
-
-const arr = ['cat', 'dog', 'fish'];
-arr.forEach(element => {
-  console.log(element);
-});
-
