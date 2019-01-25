020f57db1941652871c00eb638b1c3e61c7b87a9
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-25 01:41:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ var array = []
 array.forEach(doToElementsInArray())
   
 
-
+ it('invokes the passed-in callback function on every element of the passed-in array using Array.prototype.forEach()', () => {
+      const callback = function(fruit) {
+        return `Mmmm, ${fruit}!!!`;
+      };
 
